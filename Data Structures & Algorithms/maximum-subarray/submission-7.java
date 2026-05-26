9f5be44c0c25b5a12fa8689833f72c50d98432bd
Prasad Kukade <46354102+prasadk1999@users.noreply.github.com>
2026-05-26 21:26:01 +0530

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        if(nums.length == 1){
+            sum = nums[0];
+            return sum;
+        }
+        
+        /*for(int i = 0; i < nums.length; i++){
+            int tempSum = nums[i];
+            if(tempSum > sum)
+                sum = tempSum;
+            for(int j = i+1; j < nums.length; j++){
+                tempSum += nums[j];
+                if(tempSum > sum)
+                    sum = tempSum;
+            }
+            if(tempSum > sum)
+                sum = tempSum;
+        }
+        return sum;
+        */
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            currentSum += nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+                //maxSum = Arrays.stream(nums).min().getAsInt();
+            }
+        }
+        return maxSum;
+    }
+}
